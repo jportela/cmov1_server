5ed51ca9523c4aca44801b5635a9fd4f7569a391
Daniel Cibrao <daniel.cibrao@gmail.com>
2011-10-20 23:42:23 +0100

    Envia doutores por json para o server

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111011150903) do
+ActiveRecord::Schema.define(:version => 20111012234306) do
 
   create_table "appointments", :force => true do |t|
     t.integer  "patient_id"
